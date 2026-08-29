43628b2acc339c27fe2cc4a05d762c2303841424
MaiSilvaG <mai.silvag@duocuc.cl>
2026-08-29 12:51:19 -0400

    inicio de secion sin bd

---
+++ js/login.js
@@ -0,0 +1,25 @@
+document.getElementById("btn_registrarse").addEventListener("click",registro);
+document.getElementById("btn_iniciar-secion").addEventListener("click",iniciarSecion);
+
+var formulario_login = document.querySelector(".formulario_login");
+var formulario_registro = document.querySelector(".formulario_registro");
+var contenedor_login_registro = document.querySelector(".contenedor_login-registro")
+var caja_trasera_login = document.querySelector(".caja_trasera-login")
+var caja_trasera_registro = document.querySelector(".caja_trasera-registro")
+
+function iniciarSecion(){
+    formulario_registro.style.display = "none";
+    contenedor_login_registro.style.left = "10px";
+    formulario_login.style.display = "block";
+    caja_trasera_registro.style.opacity = "1";
+    caja_trasera_login.style.opacity = "0";
+
+}
+function registro(){
+    formulario_registro.style.display = "block";
+    contenedor_login_registro.style.left = "410px";
+    formulario_login.style.display = "none";
+    caja_trasera_registro.style.opacity = "0";
+    caja_trasera_login.style.opacity = "1";
+
+}
